1c1daaddcbb113ad8bc2ede97bb183e64aaf91dd
Aviel MacBook Pro <skrypnykaviel@gmail.com>
2025-02-13 09:56:36 +0100

    Aviel : Clean up

--- Pokemon-Battle/Pokemon-Battle/Pokemon.cs
+++ Pokemon-Battle/Pokemon-Battle/Pokemon.cs
@@ -1,12 +1,4 @@
-﻿//1.The player start the game.
-//2. The player gives a name to a charmander.
-//3. The charmander does its battle cry for ten times.
-//4. The player can give a new name to the same charmander.
-//5. The charmander does its battle cry for ten times.
-//6. Repeat 4 and 5 until the player quits the game.
-
-
-using Pokemon_Battle;
+﻿using Pokemon_Battle;
 
 class Pokemon
 {
